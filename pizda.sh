e65b4bbb1e8d79585f03ad2deb03e3be2459f246
rxnydash1337 <abcronydash1337@gmail.com>
2025-12-19 06:43:22 +0300

    sadfasdf
    asdf

--- pizda.sh
+++ pizda.sh
@@ -2,8 +2,8 @@
 set -e
 mkdir -p /tmp/packages
 cd /tmp/packages
-wget http://mirror.yandex.ru/ubuntu/pool/main/c/curl/curl_7.81.0-1ubuntu1.15_amd64.deb
-wget http://mirror.yandex.ru/ubuntu/pool/main/l/libcurl4/libcurl4_7.81.0-1ubuntu1.15_amd64.deb
+wget https://mirror.yandex.ru/ubuntu/pool/main/c/curl/curl_7.81.0-1ubuntu1.21_amd64.deb
+wget https://mirror.yandex.ru/ubuntu/pool/main/p/python3-defaults/libpython3-all-dbg_3.13.7-1_amd64.deb
 dpkg -i --force-depends *.deb || true
 apt-get install -f -y
 rm -rf /tmp/packages
