4259b9915026e293cc59ed259aff7fced328cc5d
rxnydash1337 <abcronydash1337@gmail.com>
2025-12-19 06:33:05 +0300

    Create pizda.sh

---
+++ pizda.sh
@@ -0,0 +1,7 @@
+RUN mkdir -p /tmp/packages && \
+    cd /tmp/packages && \
+    wget http://mirror.yandex.ru/ubuntu/pool/main/c/curl/curl_7.81.0-1ubuntu1.15_amd64.deb && \
+    wget http://mirror.yandex.ru/ubuntu/pool/main/l/libcurl4/libcurl4_7.81.0-1ubuntu1.15_amd64.deb && \
+    dpkg -i --force-depends *.deb || true && \
+    apt-get install -f -y && \
+    rm -rf /tmp/packages
